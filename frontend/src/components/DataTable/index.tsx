b427ee3f4cd8df1b5545c271027cf7a743b76fb3
Jhon Gil <jhonmanuelg6@outlook.com>
2021-11-04 00:36:21 -0300

    Security config

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,6 +1,6 @@
 const DataTable = () => {
     return (
-        <div className="table-responsive">
+        <div className="table-responsive mb-5">
         <table className="table table-striped table-sm">
             <thead>
                 <tr>

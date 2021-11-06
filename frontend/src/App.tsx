6915ea6a71cdbf5c697f17ffdac968260cdb72d8
Jhon Gil <jhonmanuelg6@outlook.com>
2021-11-05 21:03:00 -0300

    Modificaçao o nome h5

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         </div>
 
         <div className="col-sm-6">
-          <h5 className="text-center text-secondary">taxa de sucesso (%)</h5>
+          <h5 className="text-center text-secondary">Todas vendas </h5>
           <DonutChart />
         </div>
       </div>

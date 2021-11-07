d0c9c208c951191a615a914c468458f2bec105e9
Jhon Gil <jhonmanuelg6@outlook.com>
2021-11-07 18:19:55 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,25 @@
+
+import Dashboard from "pages/Dashboard";
+import Home from "pages/Home";
+import { Switch } from "react-router-dom";
+import { Route,  } from "react-router-dom";
+import { BrowserRouter,  } from "react-router-dom";
+
+const Routes = () => {
+    return (
+      <BrowserRouter>
+         <Switch>
+           <Route path="/" exact>
+               <Home />
+           </Route>
+
+           <Route path="/dashboard" >
+               <Dashboard />
+           </Route>
+         </Switch>
+      </BrowserRouter>
+    );
+  };
+  
+  export default Routes;
+  
